3ccad30598f77950d66010b7d6fb4bdcaa930473
Kevin Kobayashi <kkobayashi156@gmail.com>
2021-07-17 13:18:04 -0600

    refactor: removed unused, tidyed components

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Tooltip from './tooltip/tooltip.component';
 import { convertColorToHex, convertColorToRGB } from './utils/utils';
 import Error from './error/error.component';
 
-function getRandomColor() {
-  const STARTING_COLORS: string[] = ['#c9f4fe', 'rgb(201, 244, 254)', '#b2f1d8', 'rgb(178, 241, 216)', '#fffee3', 'rgb(255, 254, 227)', '#ffb3c8', 'rgb(255, 179, 200)'];
-  return STARTING_COLORS[Math.floor(Math.random() * STARTING_COLORS.length)];
-}
-
 function App() {
+  function getRandomColor() {
+    const STARTING_COLORS: string[] = ['#c9f4fe', 'rgb(201, 244, 254)', '#b2f1d8', 'rgb(178, 241, 216)', '#fffee3', 'rgb(255, 254, 227)', '#ffb3c8', 'rgb(255, 179, 200)'];
+    return STARTING_COLORS[Math.floor(Math.random() * STARTING_COLORS.length)];
+  }
+
   const [color, setColor] = useState<string>(getRandomColor());
   const [error, setError] = useState<string>('');
   const [copied, setCopied] = useState(false);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,5 @@
 import { fromString } from 'css-color-converter';
 
-export function convertColor(color: string): string {
-  const HEX_REGEX = /#?([a-fA-F0-9]{8}|[a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/gi;
-
-  if (HEX_REGEX.test(color)) {
-    return fromString(color).toRgbString();
-  }
-
-  return fromString(color).toHexString();
-}
-
 export function convertColorToHex(color: string) {
   return fromString(color).toHexString();
 }

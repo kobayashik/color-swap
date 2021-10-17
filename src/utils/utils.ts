9c8a090c12d4a12b08f4312700cd82bff1771eb2
Kevin Kobayashi <kkobayashi156@gmail.com>
2021-10-16 20:00:14 -0600

    feat: added onCopy button

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,36 @@
 import { fromString } from 'css-color-converter';
+import Color from 'color';
 
-export function convertColorToHex(color: string) {
-  return fromString(color).toHexString();
-}
+export const convertColorToHex = (color: string) => fromString(color).toHexString();
 
-export function convertColorToRGB(color: string) {
-  return fromString(color).toRgbString();
-}
+export const convertColorToRGB = (color: string) => fromString(color).toRgbString();
+
+export const getContrastingColor = (color: string) => {
+  try {
+    const initial = Color(color);
+    const lightness = initial.lightness();
+    const darkened = initial.darken(0.1).fade(0.6).string();
+    const lightened = initial.lightness(lightness + 80).fade(0.8).string();
+
+    return initial.isLight() ? darkened : lightened;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return null;
+};
+
+export const getOpaqueContrastingColor = (color: string) => {
+  try {
+    const initial = Color(color);
+    const lightness = initial.lightness();
+    const darkened = initial.darken(0.8).opaquer(1).string();
+    const lightened = initial.lightness(lightness + 80).opaquer(1).string();
+
+    return initial.isLight() ? darkened : lightened;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return null;
+};
